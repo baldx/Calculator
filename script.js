3f09ada3977b66b29c900e9c5912712fda8e7d30
baldx <mr.ahmad.m2006@gmail.com>
2023-03-26 11:49:46 +0200

    add keyDown event listener

--- script.js
+++ script.js
@@ -6,4 +6,7 @@ let secondOperand = "";
 let result = null;
 const buttons = document.querySelectorAll("button");
 
-
+window.addEventListener("keydown", function(e) { //keydown event
+    const key = document.querySelector(`button[data-key="${e.keyCode}"]`); //declared key, declaration is button[data-key attribute] and calls back the keycode
+    key.click(); 
+});
