2790873fb674454dc817d6b74bba339e33eb511e
baldx <mr.ahmad.m2006@gmail.com>
2023-03-26 12:03:08 +0200

    update display

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ window.addEventListener("keydown", function(e) { //keydown event
     const key = document.querySelector(`button[data-key="${e.keyCode}"]`); //declared key, declaration is button[data-key attribute] and calls back the keycode
     key.click(); 
 });
+
+function updateDisplay() {
+    const display = document.querySelector("#display");
+    display.innerText  = displayValue;
+    if (display.length > 17) {
+        display.innerText = displayValue.substring(0, 9);
+    }
+}
