e131964febfe75a34c6bbd30359167d6aa261d96
baldx <mr.ahmad.m2006@gmail.com>
2023-03-26 11:32:27 +0200

    add variables

---
+++ script.js
@@ -0,0 +1,9 @@
+let displayValue = 0;
+let firstOperator = null;
+let secondOperator = null;
+let firstOperand = "";
+let secondOperand = "";
+let result = null;
+const buttons = document.querySelectorAll("button");
+
+
